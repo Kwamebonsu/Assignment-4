281c6d5feeda43102c0e0949259c26cda1c24aab
bonsk5852 <bonsk5852@HRH-IC0027307.wrdsb.ca>
2017-03-03 10:54:43 -0500

    Update

--- src/assignment/pkg4/A4Q7.java
+++ src/assignment/pkg4/A4Q7.java
@@ -18,16 +18,41 @@ public class A4Q7 {
     public static void main(String[] args) {
         // Create a scanner
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("You are on square 1.");
-        
+
+
         System.out.print("Enter sum of dice: ");
         double roll1 = input.nextDouble();
         double position2 = 1 + roll1;
-        if(roll1 > 1 && roll1 <13){
+        if (roll1 > 1 && roll1 < 13) {
             System.out.println("You are now on square " + position2);
+        } else if (roll1 == 0) {
+            System.out.println("You Quit!");
         }
-        
-        
+        System.out.print("Enter sum of dice: ");
+        double roll2 = input.nextDouble();
+        double position3 = position2 + roll2;
+        if (roll2 > 1 && roll2 < 13) {
+            System.out.println("You are now on square " + position3);
+        } else if (roll2 == 0) {
+            System.out.println("You Quit!");
+        }
+        System.out.print("Enter sum of dice: ");
+        double roll3 = input.nextDouble();
+        double position4 = position3 + roll3;
+        if (roll3 > 1 && roll3 < 13) {
+            System.out.println("You are now on square " + position4);
+        } else if (roll3 == 0) {
+            System.out.println("You Quit!");
+        }
+        System.out.print("Enter sum of dice: ");
+        double roll4 = input.nextDouble();
+        double position5 = position4 + roll4;
+        if (roll4 > 1 && roll4 < 13) {
+            System.out.println("You are now on square " + position5);
+        } else if (roll4 == 0) {
+            System.out.println("You Quit!");
+        }
     }
 }
---
+++ src/assignment/pkg4/SnakesandLadders.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg4;
+
+/**
+ *
+ * @author bonsk5852
+ */
+public class SnakesandLadders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
